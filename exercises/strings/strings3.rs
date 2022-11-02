df18d1507c0c6dcc51161ab59b00f687042875d8
Farbod <ftor1@student.monash.edu>
2022-11-02 21:39:12 +1100

    progress

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,21 +1,22 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
-    // TODO: Remove whitespace from both ends of a string!
-    ???
+    input.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
-    // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    format!("{} world!", input)
 }
 
 fn replace_me(input: &str) -> String {
-    // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    if let Some(idx) = input.find("cars") {
+        let output = input.to_string();
+        output[..idx].to_string() + "balloons" + &output[idx+4..].to_string()
+    } else {
+        input.to_string()
+    }
 }
 
 #[cfg(test)]
